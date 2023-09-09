fa0a26e733e482c44862523a45450fd119fe3790
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-09 20:58:33 +0100

    URL Shortner Components

---
+++ Url-Shortner/src/lib/components/Header.svelte
@@ -0,0 +1,3 @@
+<header class="py-8">
+	<h1 class="text-center text-4xl">URL.MoinJulian.com</h1>
+</header>
---
+++ Url-Shortner/src/lib/components/Loader.svelte
@@ -0,0 +1,39 @@
+<div class="loader" aria-label="loading...">
+	<div class="wave" />
+	<div class="wave" />
+</div>
+
+<style>
+	.loader {
+		--size: 3rem;
+		--duration: 1.6s;
+		display: flex;
+		justify-content: center;
+		height: var(--size);
+		margin-block: 0.5rem;
+	}
+
+	.wave {
+		width: var(--size);
+		height: var(--size);
+		position: absolute;
+		border-radius: 50%;
+		border: 0.2rem solid white;
+		animation: ripple var(--duration) ease-out infinite;
+	}
+
+	.wave:nth-child(2) {
+		animation-delay: calc(-0.5 * var(--duration));
+	}
+
+	@keyframes ripple {
+		from {
+			scale: 0;
+			opacity: 1;
+		}
+		to {
+			scale: 1;
+			opacity: 0;
+		}
+	}
+</style>
