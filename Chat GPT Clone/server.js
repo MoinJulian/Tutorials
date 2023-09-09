84242514e27a0ababf91ddab5ad1b624d99618aa
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-09 21:14:31 +0100

    Add enviromental variable and request

--- Chat GPT Clone/server.js
+++ Chat GPT Clone/server.js
@@ -1,3 +1,4 @@
+//setup express
 import express from "express";
 const app = express();
 
@@ -7,6 +8,17 @@ app.listen(PORT, () => {
   console.log("Server is listening on PORT 3000");
 });
 
-// app.get("/", (req, res) => res.send("Hi, server is running"));
-
+// middleware to serve static public, and use json to send data
 app.use(express.static("public"));
+app.use(express.json());
+
+// add enviroment variables
+import dotenv from "dotenv";
+dotenv.config();
+
+// handle POST request
+app.post("/completion", (req, res) => {
+  console.log(req.body.message);
+});
+
+
