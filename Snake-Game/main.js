cb456c54d59cbc90b24b672d00e1bfdc59ca45ce
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-10 08:48:48 +0100

    Genarated cells

--- Snake-Game/main.js
+++ Snake-Game/main.js
@@ -1,1 +1,9 @@
-console.log("Hi Mom");
+const mainElement = document.querySelector("main");
+
+for (let i = 0; i < 20; i++) {
+  for (let j = 0; j < 20; j++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    mainElement.appendChild(cell);
+  }
+}
