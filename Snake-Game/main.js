fccdf9ccea89d57fc7ae842dcff75e2fb27e4862
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-10 09:01:00 +0100

    draw Snake

--- Snake-Game/main.js
+++ Snake-Game/main.js
@@ -1,9 +1,25 @@
 const mainElement = document.querySelector("main");
 
-for (let i = 0; i < 20; i++) {
-  for (let j = 0; j < 20; j++) {
+for (let y = 0; y < 20; y++) {
+  for (let x = 0; x < 20; x++) {
     const cell = document.createElement("div");
     cell.className = "cell";
+    cell.id = y + "," + x;
     mainElement.appendChild(cell);
   }
 }
+
+let snake = [
+  [4, 5],
+  [5, 5],
+  [6, 5],
+];
+
+function drawSnake() {
+  for (const [x, y] of snake) {
+    const cell = document.getElementById(y + "," + x);
+    cell.classList.add("active");
+  }
+}
+
+drawSnake();
