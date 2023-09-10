126780a58a7821aa5917cd03e5f1c238b11121b8
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-10 21:08:00 +0100

    Upadted links

--- Landing-Page/src/App.svelte
+++ Landing-Page/src/App.svelte
@@ -1,13 +1,17 @@
 <main>
   <h1>This is the Landing Page for all my Tutorial Projects</h1>
 
+  <p>The most of the Turtorials are made by <a href="https://youtube.com/@scriptraccoon" target="_blank">Script Raccoon</a>.</p>
+
 <p>Here you can find all my Tutorial Projects, for Example:</p>
 
 <ul>
-  <li>Particle Animation</li>
-  <li>Snake Game</li>
-  <li>Url Shortner</li>
+  <li><a href="https://particles.moinjulian.com/" target="_blank">Particle Animation</a></li>
+  <li><a href="https://snake.moinjulian.com/" target="_blank">Snake Game</a></li>
+  <li><a href="https://url.moinjulian.com" target="_blank">Url Shortner</a></li>
 </ul>
+<br>
+<p class="error"> The repository can be found on <a href="https://github.com/moinjulian" target="_blank">GitHub</a> </p>
 </main>
 
 <style>
