b269f11fec07ffdea52c91548b82b0d2961b311a
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-10 20:57:57 +0100

    renamed folder

--- Landing-Page/src/App.svelte
+++ Landing-Page/src/App.svelte
@@ -1,47 +1,23 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
-
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>This is the Landing Page for all my Tutorial Projects</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+<p>Here you can find all my Tutorial Projects, for Example:</p>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<ul>
+  <li>Particle Animation</li>
+  <li>Snake Game</li>
+  <li>Url Shortner</li>
+</ul>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    max-width: 40vw;
+    margin-inline: auto;
+    padding-inline: 1rem;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  li {
+    margin-left: 20px;
   }
 </style>
