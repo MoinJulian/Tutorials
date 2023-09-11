b49d826afd8616e9415f3c3467cc3bde29d88409
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-11 21:28:56 +0100

    Answers Svelte

--- Quiz-App-With-Svelte-and-Vue/svelte/src/components/Question.svelte
+++ Quiz-App-With-Svelte-and-Vue/svelte/src/components/Question.svelte
@@ -1,5 +1,65 @@
 <script lang="ts">
-    export let question: question;
+  import { questions } from "../config";
+
+  export let question: question;
+  let selected_index = 0;
 </script>
 
-<h2>{question.question}</h2>
+<form>
+  <h2>{question.question}</h2>
+  <section class="answers">
+    {#each question.answers as answer, index}
+      <label class="answer" class:selected={index == selected_index}>
+        <input
+          type="radio"
+          value={index}
+          name={question.id}
+          bind:group={selected_index}
+        />
+        <span class="answer_text">
+          {answer}
+        </span>
+      </label>
+    {/each}
+  </section>
+</form>
+
+<style>
+  h2 {
+    padding-block: 2rem;
+  }
+
+  .answers {
+    display: flex;
+    flex-direction: column;
+    gap: 0.75rem;
+  }
+
+  .answer {
+    background-color: var(--answer-color);
+    padding: 1rem;
+    border-radius: 0.25rem;
+    font-size: 1.25rem;
+    cursor: pointer;
+  }
+
+  .answer.selected {
+    background-color: var(--answer-color-selected);
+    outline: 0.1rem solid var(--outline-color);
+  }
+
+  .answer_text {
+    padding-left: 0.5rem;
+  }
+
+  menu {
+    margin-top: 1.5rem;
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  .message {
+    font-size: 1.25rem;
+    margin-top: 1.5rem;
+  }
+</style>
