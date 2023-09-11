14f4d12b6f474ce5891a3addd2497c12a2e3391e
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-11 21:22:50 +0100

    Answers Vue

--- Quiz-App-With-Svelte-and-Vue/vue/src/components/Question.vue
+++ Quiz-App-With-Svelte-and-Vue/vue/src/components/Question.vue
@@ -1,7 +1,68 @@
 <script setup lang="ts">
+import { ref } from "vue";
 const { question } = defineProps<{ question: question }>();
+let selected_index = ref(0);
 </script>
 
 <template>
-  <h2>{{ question.question }}</h2>
+  <form>
+    <h2>{{ question.question }}</h2>
+    <sectio class="answers">
+      <label
+        class="answer"
+        v-for="(answer, index) in question.answers"
+        :class="{ selected: index == selected_index }"
+      >
+        <input
+          type="radio"
+          :value="index"
+          :name="question.id"
+          v-model="selected_index"
+        />
+        <span class="answer_text">
+          {{ answer }}
+        </span>
+      </label>
+    </sectio>
+  </form>
 </template>
+
+<style scoped>
+h2 {
+  padding-block: 2rem;
+}
+
+.answers {
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+}
+
+.answer {
+  background-color: var(--answer-color);
+  padding: 1rem;
+  border-radius: 0.25rem;
+  font-size: 1.25rem;
+  cursor: pointer;
+}
+
+.answer.selected {
+  background-color: var(--answer-color-selected);
+  outline: 0.1rem solid var(--outline-color);
+}
+
+.answer_text {
+  padding-left: 0.5rem;
+}
+
+menu {
+  margin-top: 1.5rem;
+  display: flex;
+  gap: 0.5rem;
+}
+
+.message {
+  font-size: 1.25rem;
+  margin-top: 1.5rem;
+}
+</style>
