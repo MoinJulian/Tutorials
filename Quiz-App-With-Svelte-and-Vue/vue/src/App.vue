031815d130780e3318b279e1433e882c3ff49b7e
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-11 20:59:56 +0100

    Add questions vue

--- Quiz-App-With-Svelte-and-Vue/vue/src/App.vue
+++ Quiz-App-With-Svelte-and-Vue/vue/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Header from "./components/Header.vue";
+import { questions } from "./config";
 </script>
 
 <template>
   <Header></Header>
+  {{ JSON.stringify(questions) }}
 </template>
---
+++ Quiz-App-With-Svelte-and-Vue/vue/src/config/index.ts
@@ -0,0 +1,55 @@
+export const questions: question[] = [
+  {
+    id: "what",
+    question: "Vue.js is a ...",
+    answers: ["JavaScript runtime", "Design pattern", "JavaScript framework"],
+    correct_answer_index: 2,
+  },
+  {
+    id: "when",
+    question: "In which year was Vue.js initially published?",
+    answers: ["2013", "2014", "2015"],
+    correct_answer_index: 1,
+  },
+  {
+    id: "who",
+    question: "Who invented Vue.js?",
+    answers: ["Eyou Van", "Evan You", "Ian Vue"],
+    correct_answer_index: 1,
+  },
+  {
+    id: "inspire",
+    question: "When creating Vue.js, Evan You was inspired by ...",
+    answers: ["AngularJS", "jQuery", "React"],
+    correct_answer_index: 0,
+  },
+
+  {
+    id: "spa",
+    question: "With Vue.js we can create SPAs. What does SPA stand for?",
+    answers: [
+      "Speed-Provisioned Application",
+      "Sandbox-Powered Application",
+      "Single-Page Application",
+    ],
+    correct_answer_index: 2,
+  },
+  {
+    id: "meta",
+    question: "Which meta-framework is based on Vue.js?",
+    answers: ["Naxt", "Next", "Nixt", "Nuxt"],
+    correct_answer_index: 3,
+  },
+  {
+    id: "loops",
+    question: "Which directive is used to render loops in Vue.js?",
+    answers: ["v-for", "v-in", "v-each"],
+    correct_answer_index: 0,
+  },
+  {
+    id: "lang",
+    question: "The source code of Vue.js is written in ...",
+    answers: ["Rust", "TypeScript", "Go"],
+    correct_answer_index: 1,
+  },
+];
