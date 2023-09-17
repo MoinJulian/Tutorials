01bd63d1c5583720b83ea3920b95e2914a252b49
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-17 20:01:42 +0100

    Add Rectangle Class

---
+++ Jump-and-Run-Template/js/main.js
@@ -0,0 +1,5 @@
+import { Rectangle } from "./objects/Rectangle.js";
+
+const r = new Rectangle({ pos: [10, 10], size: [300, 20], color: "green" });
+
+r.draw();
---
+++ Jump-and-Run-Template/js/objects/Rectangle.js
@@ -0,0 +1,47 @@
+import { ctx } from "../canvas.js";
+
+export class Rectangle {
+  constructor(options) {
+    this.pos = options.pos;
+    this.size = options.size;
+    this.color = options.color;
+    this.type = "Rectangle";
+  }
+
+  get left() {
+    return this.pos[0];
+  }
+
+  get right() {
+    return this.pos[0] + this.size[0];
+  }
+
+  get top() {
+    return this.pos[1];
+  }
+
+  get bottom() {
+    return this.pos[1] + this.size[1];
+  }
+
+  setLeft(val) {
+    this.pos[0] = val;
+  }
+
+  setRight(val) {
+    this.pos[0] = val - this.size[0];
+  }
+
+  setTop(val) {
+    this.pos[1] = val;
+  }
+
+  setBottom(val) {
+    this.pos[1] = val - this.size[1];
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.pos[0], this.pos[1], this.size[0], this.size[1]);
+  }
+}
